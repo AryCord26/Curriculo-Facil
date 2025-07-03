63919bbf609be59e9463142a4d52d4516f4e95d0
Ary Cordeiro <ary_cordeiro@outlook.com>
2025-07-03 16:47:32 -0300

    Update EducationForm.jsx

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,1 +1,26 @@
+// src/components/EducationForm.jsx
+import React from 'react';
 
+function EducationForm() {
+  return (
+    <section>
+      <h2>Formação Acadêmica</h2>
+      <form className="form-section">
+        <label>
+          Curso:
+          <input type="text" name="curso" />
+        </label>
+        <label>
+          Instituição:
+          <input type="text" name="instituicao" />
+        </label>
+        <label>
+          Ano de Conclusão:
+          <input type="text" name="anoConclusao" />
+        </label>
+      </form>
+    </section>
+  );
+}
+
+export default EducationForm;
