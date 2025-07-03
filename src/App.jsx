83f94a8a38a1b8217e6e9dbfac89637710aee8d3
Ary Cordeiro <ary_cordeiro@outlook.com>
2025-07-03 16:59:29 -0300

    Update App.jsx
    Melhora no Preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 // src/App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import BasicInfoForm from './components/BasicInfoForm';
+import ObjectiveForm from './components/ObjectiveForm';
+import ResumePreview from './components/ResumePreview';
 
 function App() {
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    telefone: '',
+    linkedin: '',
+    objetivo: '',
+  });
+
   return (
     <div className="app-container">
       <h1>Crie seu primeiro currículo</h1>
-      <BasicInfoForm />
+      <BasicInfoForm formData={formData} setFormData={setFormData} />
+      <ObjectiveForm formData={formData} setFormData={setFormData} />
+      <ResumePreview data={formData} />
     </div>
   );
 }
