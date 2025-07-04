f45f7c316265a9ee9aa7eb181fc218c6efdb4362
Ary Cordeiro <ary_cordeiro@outlook.com>
2025-07-04 09:40:08 -0300

    Update DownloadButton.jsx

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,20 +1,14 @@
 // src/components/DownloadButton.jsx
 import React, { useRef } from 'react';
 import ReactToPrint from 'react-to-print';
-import ResumePreview from './ResumePreview';
 
-function DownloadButton() {
-  const componentRef = useRef();
-
+function DownloadButton({ resumeRef }) {
   return (
     <div style={{ marginTop: '2rem', textAlign: 'center' }}>
       <ReactToPrint
         trigger={() => <button>Baixar Currículo em PDF</button>}
-        content={() => componentRef.current}
+        content={() => resumeRef.current}
       />
-      <div style={{ display: 'none' }}>
-        <ResumePreview ref={componentRef} />
-      </div>
     </div>
   );
 }
